0dd7fa007b3e8f4ec8791acc424e77c43a00af66
Sam H <sam@Sams-MacBook-Pro.local>
2017-01-18 13:22:43 +0200

    Something other than initial commit

---
+++ web_server.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var express = require('express');
+var https = require('https');
+var key = fs.readFileSync('./key.pem');
+var cert = fs.readFileSync('./cert.pem')
+var https_options = {
+    key: key,
+    cert: cert
+};
+
+var PORT = 8000;
+var HOST = 'localhost';
+app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+server = https.createServer(https_options, app).listen(PORT, HOST);
+console.log('HTTPS Server listening on %s:%s', HOST, PORT);
+
